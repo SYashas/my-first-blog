c8e44bf1085e797f545ac0f5a6fd9edd054944e1
Yashas S <syashas77@gmail.com>
2018-10-09 18:33:24 +0530

    Created views to add/edit Posts by logged in user in the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.post_list, name ='post_list'),
     path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
-    path('posts/', views.all_posts, name = 'all_posts')
+    path('posts/', views.all_posts, name = 'all_posts'),
+    path('post/new/', views.post_new, name = 'post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name = 'post_edit'),
 ]
